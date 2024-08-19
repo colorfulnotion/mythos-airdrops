c790aa7957fcaf342612a32ef4714f15a3533a5f
Sourabh Niyogi <sourabhniyogi@users.noreply.github.com>
2024-08-18 21:43:18 -0700

    Scripts for MYTH airdrop v4 (final)
    The following scripts have been developed:
    * airdrop-generateBatches.js - Maps active users into the raw call data in the 643 directory, where the filename contains the call hash.  Files are in 14 groups (b1..b14)
    * airdrop-asMulti.js - For all the files in the 643 directory, uses the "asMulti" to submit the group of call batches.
    * airdrop-approveAsMulti.js - For all the files in the 643 directory, uses the "approveAsMulti" to approve the group of call batches.
    
    Using the airdrop-generateBatches.js and the Active and Inactive Users data from https://dune.com/substrate/mythos-airdrop-v4 -- we ran airdrop-generateBatches.js to populate the 643 directory with raw call data and filenames.  The filename contains the call hash (the hash of the call data), supporting verification trivially.
    
    ```
    GROUP 1: 748960 MYTH to 59365 addresses
    GROUP 2: 748960 MYTH to 59365 addresses
    GROUP 3: 748960 MYTH to 59365 addresses
    GROUP 4: 748960 MYTH to 59365 addresses
    GROUP 5: 748960 MYTH to 59365 addresses
    GROUP 6: 750003 MYTH to 57067 addresses
    GROUP 7: 751020 MYTH to 42513 addresses
    GROUP 8: 748650 MYTH to 40215 addresses
    GROUP 9: 747060 MYTH to 18767 addresses
    GROUP 10: 727639 MYTH to 13405 addresses
    GROUP 11: 675561 MYTH to 4979 addresses
    GROUP 12: 509178 MYTH to 1532 addresses
    GROUP 13: 750282 MYTH to 766 addresses
    GROUP 14: 708957 MYTH to 612 addresses
    ```
    
    The files in this directory are used to drive airdrop-asMulti.js and airdrop-approveAsMulti.js which can be run by a member of a multisig (e.g. 2/3) and then approved by another member.

---
+++ airdrop-approveAsMulti.js
@@ -0,0 +1,72 @@
+const { ApiPromise, WsProvider, Keyring } = require('@polkadot/api');
+const { cryptoWaitReady, blake2AsHex } = require('@polkadot/util-crypto');
+const fs = require('fs');
+const path = require('path');
+
+const wallet1Mnemonic = fs.readFileSync(`${process.env.HOME}/.wallet1`, 'utf-8').trim(); // ST
+const wallet2Address = '13NCLd3foNpsv1huPDzvvfyKh37NEEkGFotZnP52CTR98YFJ';   // CN
+const wallet3Address = '14QT3gXBmzj8ZqwtKrxGHvsyXUL1m1hcT8rqUfe78thmB29j';   // OGW
+const production = false;
+
+async function main() {
+    // Initialize the API and wait until ready
+    const wsProvider = new WsProvider('wss://polkadot-asset-hub-rpc.polkadot.io');
+    const api = await ApiPromise.create({ provider: wsProvider });
+    await cryptoWaitReady();
+
+    if ( process.argc < 3 ) {
+	console.log("Need group input")
+	process.exit(1);
+    }
+    const group = process.argv[2];
+    console.log("Processing group: ", group)
+    
+    // Initialize keyring and add accounts
+    const keyring = new Keyring({ type: 'sr25519' });
+    const wallet1 = keyring.addFromMnemonic(wallet1Mnemonic);
+    const threshold = 2; // 2 out of 3
+    // Fixed 2nd+3rd wallet address
+    const otherSignatories = [wallet2Address, wallet3Address].sort();
+    const airdrop = "643";
+
+    // Directory containing the .txt files
+    const directoryPath = path.join(__dirname, airdrop, group);
+    // Get all .txt files from the directory
+    const files = fs.readdirSync(directoryPath).filter(file => file.endsWith('.txt'));
+    let nbatches = 0;
+    for (const file of files) {
+        // Read file content into a string
+        const filePath = path.join(directoryPath, file);
+        const hexString = fs.readFileSync(filePath, 'utf-8').trim();
+
+        // Create a call using the hex string (considering it's already encoded properly)
+        const batch = api.createType('Call', hexString);
+	const blake2Hash = blake2AsHex(batch.toHex());
+	if ( file.includes(blake2Hash) ) { // CHECKS that the hash of the content of the file is in the filename
+            // Approve the transaction using the third wallet address
+            const approve = api.tx.multisig.approveAsMulti(
+		threshold,
+		otherSignatories,
+		null,
+		blake2Hash,
+		0
+            );
+            // Send the transaction from the first wallet
+            if ( production ) {
+		const { blockHash } = await multisig.signAndSend(wallet1, { nonce: -1 });
+		console.log(`Batch ${nbatches}/${files.length} approveAsMulti submitted with block hash ${blockHash}`);
+	    } else {
+		console.log(`Batch ${nbatches}/${files.length}: ${file} READY FOR PRODUCTION!`);
+		nbatches++;
+	    }
+	} else {
+	    console.log(`Batch ${nbatches}/${files.length}: ${file} HASH check FAILED!`);
+	    process.exit(0);
+	}
+    }
+
+    // Disconnect from the node
+    await api.disconnect();
+}
+
+main().catch(console.error);
---
+++ airdrop-generateBatches.js
@@ -0,0 +1,124 @@
+const { ApiPromise, WsProvider, Keyring } = require('@polkadot/api');
+const { cryptoWaitReady } = require('@polkadot/util-crypto');
+const fs = require('fs');
+const path = require('path');
+const csv = require('csv-parser');
+
+// Function to read CSV file asynchronously
+async function readCsvFile(filePath) {
+    return new Promise((resolve, reject) => {
+        const results = [];
+        fs.createReadStream(filePath)
+            .pipe(csv())
+            .on('data', (data) => results.push(data))
+            .on('end', () => resolve(results))
+            .on('error', reject);
+    });
+}
+
+async function main() {
+    // Initialize the API and wait until ready
+    const wsProvider = new WsProvider('wss://polkadot-asset-hub-rpc.polkadot.io');
+    const api = await ApiPromise.create({ provider: wsProvider });
+    await cryptoWaitReady();
+
+    // Load the target addresses and amounts from CSV
+    const inactivesData = await readCsvFile('mythos-airdrop-v4-inactiveusers-20240818.csv');
+    console.log(`Read ${inactivesData.length} inactive addresses`)
+    let activesData = await readCsvFile('mythos-airdrop-v4-activeusers-20240818.csv');
+    console.log(`Read ${activesData.length} active addresses`)
+    // sort in ascending order
+    activesData = activesData.sort((a, b) => a.airdrop_amount - b.airdrop_amount);
+    
+    const inactiveAmount = '12000000000000000000'; // 12 MYTH
+    var transfersData = [];
+    for ( const inactive of inactivesData ) {
+	transfersData.push({ address_ss58: inactive.address_ss58, airdrop_amount: inactiveAmount })
+    }
+    for ( const active of activesData ) {
+	transfersData.push(active);
+    }
+    console.log(`TOTAL: ${transfersData.length} addresses`)
+    
+    // Define maximum number of calls in a single batch
+    const MAX_CALLS_PER_BATCH = 383;
+    const MAX_MYTH_PER_GROUP = 752000;
+
+    // Additional transfer amount and address
+    const additionalTransferAddress = '13NCLd3foNpsv1huPDzvvfyKh37NEEkGFotZnP52CTR98YFJ';
+    const additionalTransferAmount = '236'; 
+    const decimalString = '000000000000000000'; // 18 zeroes for MYTH
+    // Split transfers into multiple batches
+    const airdrop = '643';
+    const batches = [];
+    let asset = {
+	parents: 1,
+	interior: {
+	    X1: {
+		parachain: 3369
+	    }
+	}
+    }
+
+    // write batches
+    let batchAmount = {};
+    let batchCount = {};
+    for (let i = 0, batch = 0 ; i < transfersData.length; i += MAX_CALLS_PER_BATCH, batch++) {
+	let totalAmount = 0.0;
+	const batchCalls = transfersData.slice(i, i + MAX_CALLS_PER_BATCH).map(({ address_ss58, airdrop_amount }) => {
+	    if ( airdrop_amount == inactiveAmount ) {
+		totalAmount += 12.0;
+		return api.tx.foreignAssets.transfer(asset, address_ss58, inactiveAmount);
+	    } else {
+		totalAmount += Math.round(airdrop_amount);
+		return api.tx.foreignAssets.transfer(asset, address_ss58, `${airdrop_amount}${decimalString}`);
+	    }
+	});
+        if ( additionalTransferAmount < 240 ) { // safety
+	    batchCalls.push(api.tx.foreignAssets.transfer(asset, additionalTransferAddress, `${additionalTransferAmount}${decimalString}`));
+	    totalAmount += Math.round(additionalTransferAmount);
+	    const batchIndexHex = `${batch.toString(16).padStart(4, '0')}`;
+            batchCalls.push(api.tx.system.remark(`${batchIndexHex}:${totalAmount}`));
+	}
+        batches.push(api.tx.utility.batch(batchCalls));
+	batchAmount[batch] = totalAmount;
+	batchCount[batch] = batchCalls.length - 2;
+    }
+    let grouptally = {};
+    let groupcount = {};
+    let group = 1;
+    for (let i = 0; i < batches.length; i++) {
+	const batchIndexHex = `${i.toString(16).padStart(4, '0')}`;
+        const batch = batches[i];
+
+        // get callHash and data
+        const callHash = batch.method.hash.toHex();
+        const callData = batch.method.toHex();
+
+        // Write callData to file fn
+	if ( grouptally[group] + batchAmount[i] > MAX_MYTH_PER_GROUP ) {
+	    console.log(`GROUP ${group}: ${grouptally[group]} MYTH to ${groupcount[group]} addresses`);
+	    group++;
+	}
+	let groupdir = path.join(airdrop, `b${group}`);
+	if ( grouptally[group] == undefined ) {
+	    grouptally[group] = batchAmount[i];
+	    groupcount[group] = batchCount[i];
+	    fs.mkdirSync(groupdir, { recursive: true });
+	} else {
+	    grouptally[group] += batchAmount[i];
+	    groupcount[group] += batchCount[i];
+	}
+	let fn = path.join(groupdir, `${batchIndexHex}-${callHash}-${batchAmount[i]}-${batchCount[i]}.txt`);
+        fs.writeFileSync(fn, callData);
+	console.log(`Wrote ${fn} (${i}/${batches.length}): ${batchAmount[i]} MYTH with ${batchCount[i]} addresses`);
+	if ( i == batches.length - 1 ) {
+	    console.log(`GROUP ${group}: ${grouptally[group]} MYTH to ${groupcount[group]} addresses`);
+	}
+    }
+
+    // Disconnect from the node
+    await api.disconnect();
+}
+
+main().catch(console.error);
